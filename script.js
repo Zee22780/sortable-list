5cb2772774b830f532e3844b40366f02c1dbadf2
zuri227 <zuri22780@gmail.com>
2022-06-10 20:13:21 -0400

    Add check order function

--- script.js
+++ script.js
@@ -39,6 +39,7 @@ function addEventListener() {
   })
 }
 
+check.addEventListener('click', checkOrder)
 /*-------------------------------- Functions --------------------------------*/
 createList()
 
@@ -69,21 +70,48 @@ function createList(){
 }
 
 function dragStart() {
-
+  dragStartIndex = +this.closest('li').getAttribute('data-index')
 }
 
 function dragEnter() {
   this.classList.add('over')
 }
 
-function dragOver() {
-  
+function dragOver(e) {
+  e.preventDefault()
 }
 
 function dragDrop() {
-  
+  const dragEndIndex = +this.getAttribute('data-index')
+
+  swapItems(dragStartIndex, dragEndIndex)
+
+  this.classList.remove('over')
 }
 
 function dragLeave() {
   this.classList.remove('over')
 }
+
+function swapItems(fromIndex, toIndex) {
+  const itemOne = listItems[fromIndex].querySelector('.draggable')
+  const itemTwo = listItems[toIndex].querySelector('.draggable')
+
+  listItems[fromIndex].appendChild(itemTwo)
+  listItems[toIndex].appendChild(itemOne)
+}
+
+function checkOrder() {
+  listItems.forEach((listItem, index) => {
+    const placeName = listItem.querySelector('.draggable').innerText.trim()
+
+    if(placeName !== bestPlaces[index]){
+      listItem.classList.add('wrong')
+    }
+    else {
+      listItem.classList.remove('wrong')
+      listItem.classList.add('right')
+    }
+  })
+}
+
